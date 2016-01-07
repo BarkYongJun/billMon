05311d7a928f3b87d362715f09a105e231451e9a
BarkYongJun <elpincerss@gmail.com>
2016-01-07 15:44:44 +0900

    변경 추가
    코드가 부족해서 추가해드립니다.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,8 @@
 class HelloWorld {
   public static void main(String[] args) {
     System.out.println("이렇게 쓰세요");
+    //박용준 2016-01-07 기능 추가
+    System.out.println("라영씨 바보 ㅋ");
+    //이렇게 쓰면 됩니다.
     }
 }
