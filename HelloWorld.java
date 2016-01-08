204b0b8495153b19043daea48a3cfc6c70090007
BarkYongJun <elpincerss@gmail.com>
2016-01-08 14:44:44 +0900

    16-01-08
    갱신
    누계합 추가

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,12 @@ class HelloWorld {
   public static void main(String[] args) {
     System.out.println("이렇게 쓰세요");
     //박용준 2016-01-07 기능 추가
-    System.out.println("라영씨 바보 ㅋ");
-    //이렇게 쓰면 됩니다.
+    while(true) {
+      //이렇게 쓰면 됩니다.
+      System.out.println("라영씨 바보 ㅋ");
+      List<Int> babo = Arrays.asList{1, 2, 3,};
+      int i=0;
+      int sum += babo.get(i);
     }
+  }
 }
